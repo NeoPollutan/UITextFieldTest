9b6a406f452399f04f2d35593c5c91220fd94f9e
Rizky Adipratama <adipratama.rizky@gmail.com>
2019-05-16 10:17:23 +0700

    Initial Commit

---
+++ UITextFieldTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITextFieldTest
+//
+//  Created by Rizky Adipratama Ruddyar on 16/05/19.
+//  Copyright © 2019 Rizky Adipratama Ruddyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
